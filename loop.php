335499c07a9704a1198ff5bc74cc14dc4bb94dfa
Yash Prajapati <yashypsoft@gmail.com>
2020-01-20 09:52:53 +0530

    while ,for and do while loop practice

---
+++ loop.php
@@ -0,0 +1,22 @@
+<?php
+
+    $counter =1;
+    //while loop
+    while($counter <10){
+        echo "$counter hello<br>";
+        $counter++;
+    }
+
+    //do while loop
+    do{
+        echo "hello <br>";
+        $counter++;
+    }while($counter<20);
+
+    //for loop
+    for($count = 1; $count<10 ; $count++ ){
+        echo "Yash<br>";
+    }
+
+
+?>
