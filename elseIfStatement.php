d5d57b8fb27f6269d4a492502e8deb1777cd7be2
Yash Prajapati <yashypsoft@gmail.com>
2020-01-20 09:49:37 +0530

    else if statement practice

---
+++ elseIfStatement.php
@@ -0,0 +1,23 @@
+<?php
+
+    $num =11;
+
+    if ($num==11) {
+        echo 'num is 11 <br>';
+    }
+    else if($num == 12){
+        echo 'num is 12';
+    }else{
+        echo 'number is not 11 and 12'; 
+    }
+
+
+    $num1=11;
+    $num2=14;
+
+    if($num1>=11 && $num1<=15){
+        echo 'Number between 11 to 15';
+    }
+
+
+?>
