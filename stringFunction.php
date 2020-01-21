3fdefd341cdd220c8dd958d4f72fee7b85d64660
Yash Prajapati <yashypsoft@gmail.com>
2020-01-21 17:52:41 +0530

    string dowhile fix

--- stringFunction.php
+++ stringFunction.php
@@ -1,52 +1,36 @@
 <?php
 
-$sentance = "Hello Everyone.";
+$sentance = "Hellos.";
 $lengthOfSentence = strlen($sentance);
 
 echo $lengthOfSentence;
 echo '<br>';
-for($i=0;$i<$lengthOfSentence;$i++)
-{
-    echo nl2br("Something is repeat\n");
+for ($i = 0; $i < $lengthOfSentence; $i++) {
+    echo nl2br("Heyyyyy\n");
 }
 
 //lower and upper string
-
 $lower = strtolower($sentance);
 $upper = strtoupper($sentance);
 
-echo $lower .'<Br>'. $upper .'<br>';
+echo $lower . '<Br>' . $upper . '<br>';
 
 //
 $offset = 0;
-$paragraph = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores, in.';
-$find  = "l";
+$paragraph = 'Lorem, ipsum dolor sitL amet consecLtetur adipisicing elit. Asperiores, in.';
+$find  = "L";
 $size = strlen($find);
 
-while($position = strpos($paragraph,$find,$offset))
-{
+do {
+    $position = strpos($paragraph, $find, $offset);
     echo "string found at $position <br> ";
-    $offset = $position +$size;
-
-
-}
-
+    $offset = $position + $size;
+} while ($position = strpos($paragraph, $find, $offset));
 
 //string replace
-
 $string = "Welcome to Cybercom creation";
-$find = ['a','e','i'];
-$replace = ['A','E','I'];
-$newString = str_replace($find,$replace,$string,$i);
+$find = ['a', 'e', 'i'];
+$replace = ['A', 'E', 'I'];
+$newString = str_replace($find, $replace, $string, $i);
 
 echo "New string = <strong>  $newString  </strong>and how many position replaced $i";
-
-
-
-
-
-
-
-
-
-?>
