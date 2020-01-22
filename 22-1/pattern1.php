c1e141942439ef4c7067900146a3007417e853ce
Yash Prajapati <yashypsoft@gmail.com>
2020-01-22 17:49:17 +0530

    add to table structure

--- 22-1/pattern1.php
+++ 22-1/pattern1.php
@@ -1,5 +1,4 @@
-<?php
-/*
+<!-- /*
 Pattern 1
 * * * * * * * * * * * * * * * * *
 * * * * * * * * * * * * * * *
@@ -11,17 +10,39 @@ Pattern 1
 * * *
 *
 
-*/
+*/ -->
 
 
 
-function displayPattern($line)
-{
-    for ($i = $line + 8; $i >= 0; $i -= 2) {
-        for ($j = 0; $j < $i; $j++) {
-            echo '* ';
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <table border="1">
+        <?php
+        function displayPattern($line)
+        {
+            for ($i = $line + 8; $i >= 0; $i -= 2) {
+                echo '<tr>';
+
+                for ($j = 0; $j < $i; $j++) {
+                    echo '<td>* </td>';
+                }
+
+                echo '</tr>';
+            }
         }
-        echo ' <br>';
-    }
-}
-displayPattern(9);
+        displayPattern(9);
+
+        ?>
+    </table>
+</body>
+
+</html>
