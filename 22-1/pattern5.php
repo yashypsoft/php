7c7d2f880811004720b9097fe5233ff1eb622859
Yash Prajapati <yashypsoft@gmail.com>
2020-01-22 17:01:55 +0530

    fix pattern

--- 22-1/pattern5.php
+++ 22-1/pattern5.php
@@ -3,12 +3,13 @@ function displayPattern($row, $col)
 {
     $k = 1;
     for ($i = 1; $i <= $row; $i++) {
+        $k = $i;
         for ($j = 1; $j <= $col; $j++) {
             echo $k . ' ';
-            $k++;
+            $k+= $row;
         }
         echo '<br>';
     }
 }
 
-displayPattern(5, 5);
+displayPattern(5, 3);
