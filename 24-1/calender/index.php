66de7b3c04700d84750d8a3405fd67a341028f82
Yash Prajapati <yashypsoft@gmail.com>
2020-01-24 12:07:32 +0530

    image upload

--- 24-1/calender/index.php
+++ 24-1/calender/index.php
@@ -1,12 +1,24 @@
 <?php
-function display($month, $year)
+function display($month, $year, $name)
 {
     $timestamp = mktime(null, null, null, $month, 1, $year);
     $today = date('d-m-Y', $timestamp);
     $z = (getdate($timestamp)['wday']);
-    // print_r(getdate($timestamp));
     $lastDate = date("t", $timestamp);
+    $extension = substr($name, strpos($name, '.') + 1);
+    $type = @$_FILES['file']['type'];
+    $temp = @$_FILES['file']['tmp_name'];
 
+    if ($extension == 'jpg' || $extension == "jpeg" && $type == "image/jpeg") {
+        $location = 'upload/';
+        if (move_uploaded_file($temp, $location . $name)) {
+            echo "<img src='$location$name' width='240px'> '";
+        } else {
+            echo "<img src='$location$name' width='240px'> '";
+        }
+    } else {
+        echo "upload jpeg file only";
+    }
     echo '<br>';
     $k = 1;
     for ($i = 0; $i < 5; $i++) {
@@ -24,8 +36,6 @@ function display($month, $year)
             }
         }
         $z = 0;
-
-
         echo '</tr>';
     }
 }
@@ -45,12 +55,15 @@ function display($month, $year)
 
 <body>
 
-    <form action="" method="POST">
+    <form action="" method="POST" enctype="multipart/form-data">
         <label for="month">Month : </label>
         <input type="number" name="month" id="month"><br><br>
         <label for="year">Year :</label>
         <input type="number" name="year" id="year"><br><br>
+        <input type="file" name="file" id="file"><br><br>
+
         <input type="submit" value="Display">
+
     </form>
     <div>
         <table border="1px">
@@ -65,17 +78,21 @@ function display($month, $year)
             </tr>
             <?php
             session_start();
-            if ((isset($_SESSION["month"]) && isset($_SESSION['year'])) || ((isset($_POST['month']) && isset($_POST['year'])))) {
+            if (((isset($_SESSION["month"]) && isset($_SESSION['year']) && isset($_SESSION['fname'])) || (isset($_POST['month']) && isset($_FILES['file']['name']) && isset($_POST['year'])))) {
                 $_SESSION['month'] = isset($_POST['month']) ? $_POST['month'] : $_SESSION['month'];
-                $_SESSION['year'] = (isset($_POST['year'])) ? $_POST['year'] : $_SESSION['year'];
+                $_SESSION['year'] = isset($_POST['year']) ? $_POST['year'] : $_SESSION['year'];
+                $_SESSION['fname'] = isset($_FILES['file']['name']) ? $_FILES['file']['name'] : $_SESSION['fname'];
+                $name = $_SESSION['fname'];
                 $month = $_SESSION['month'];
                 $year = $_SESSION['year'];
-                if (preg_match('/^[0-9]{1}[1-2]{0,1}$/', $month) && preg_match('/^[0-9]{4}$/', $year)) {
-                    display($month, $year);
+
+                if (preg_match('/^[0-9]{1}[1-2]{0,1}$/', $month) && preg_match('/^[0-9]{4}$/', $year) && !empty($name)) {
+                    display($month, $year, $name);
                 } else {
                     echo "enter valid Month and year";
                 }
             }
+
             ?>
 
         </table>
