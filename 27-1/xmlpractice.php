f1fdc59c71c6809f1ace1de0ea6652a54fc3d10b
Yash Prajapati <yashypsoft@gmail.com>
2020-01-27 13:50:59 +0530

    xml practice

--- 27-1/xmlpractice.php
+++ 27-1/xmlpractice.php
@@ -1,13 +1,14 @@
 <?php
 
 $xml = simplexml_load_file('example.xml');
-
-// var_dump($xml);
+// print_r($xml);
 
 
 // echo $xml->teacher[1]->name . ' ' . $xml->teacher[2]->age;
 
 foreach($xml->teacher as $teacher){
-    echo $teacher->name . 'is '. $teacher->age .'Years old <br>' ;
-    // echo '<br>;'
+    echo $teacher->name . ' is '. $teacher->age .'Years old <br>' ;
+    foreach($teacher->lecture as $lecture){
+        echo $lecture->subject ." on " . $lecture->Lecturetime ."<br>";
+    }
 }
