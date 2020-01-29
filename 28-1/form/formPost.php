b941c1894c1d9410aa9f5e896f2cb6b290979e4d
Yash Prajapati <yashypsoft@gmail.com>
2020-01-29 16:25:07 +0530

    insert data in mysql

--- 28-1/form/formPost.php
+++ 28-1/form/formPost.php
@@ -1,6 +1,7 @@
 <?php
 
 session_start();
+require_once 'connect.php';
 
 function getData($category, $fieldData, $returnType = "")
 {
@@ -21,61 +22,62 @@ function getSessionData($category, $fieldData, $returnType = "")
 
 function ValidateData($section, $fieldname)
 {
-
-    $value = getData($section, $fieldname);
-    switch ($fieldname) {
-        case 'prefix':
-            return ($value != 'Prefix') ? false : true;
-            break;
-        case 'dob':
-            return !empty($value) ? false : true;
-            break;
-        case 'phoneNo':
-            return (strlen($value) == 10) ? false : true;
-            break;
-        case 'email':
-            return
-                preg_match('/^[\\w\\-]+(\\.[\\w\\-]+)*@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$/', $value)
-                ? false
-                : true;
-            break;
-        case 'firstName':
-        case 'lastName':
-             return preg_match('/^[a-zA-Z]{1,10}$/', $value)
-                ? false
-                : true;
-            break;
-        case 'address1':
-        case 'address2':
-        case 'company':
-        case 'city':
-        case 'password':
-        case 'confirmPassword':
-        case 'describeYourself':
-            return !empty($value) ? false : true;
-            break;
-        case 'contry':
-            return ($value != 'country') ? false : true;
-            break;
-        case 'postalCode':
-            return (strlen($value) == 6) ? false : true;
-            break;
-        case 'numberClient':
-            return ($value != 'Client-Week') ? false : true;
-            break;
-        case 'getintouch':
-        case 'hobbies':
-        case 'businessYear':
-            return (isset($_POST[$section][$fieldname])) ? false : true;
-            break;
+    $check = '';
+    if (isset($_POST['submit'])) {
+        $value = getData($section, $fieldname);
+        switch ($fieldname) {
+            case 'prefix':
+                return ($value != 'Prefix') ? false : true;
+                break;
+            case 'phoneNo':
+                return (strlen($value) == 10) ? false : true;
+                break;
+            case 'email':
+                return
+                    preg_match('/^[\\w\\-]+(\\.[\\w\\-]+)*@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$/', $value)
+                    ? false
+                    : true;
+                break;
+            case 'firstName':
+            case 'lastName':
+                return preg_match('/^[a-zA-Z]{1,10}$/', $value)
+                    ? false
+                    : true;
+                break;
+            case 'dob':
+            case 'address1':
+            case 'address2':
+            case 'company':
+            case 'city':
+            case 'password':
+            case 'confirmPassword':
+            case 'describeYourself':
+            case 'state':
+                return !empty($value) ? false : true;
+                break;
+            case 'contry':
+                return ($value != 'country') ? false : true;
+                break;
+            case 'postalCode':
+                return (strlen($value) == 6) ? false : true;
+                break;
+            case 'numberClient':
+                return ($value != 'Client-Week') ? false : true;
+                break;
+            case 'getintouch':
+            case 'hobbies':
+            case 'businessYear':
+                return (isset($_POST[$section][$fieldname])) ? false : true;
+                break;
+        }
         
     }
-    if (isset($_POST[$section])) {
-        setSessionData($section);
-    }
+   
 }
-
-
-
+if (isset($_POST['account']) && isset($_POST['address']) && isset($_POST['other'])) {
+    setSessionData('account');
+    setSessionData('address');
+    setSessionData('other');
+}
 
 
